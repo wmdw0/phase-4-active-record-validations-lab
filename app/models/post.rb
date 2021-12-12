9689edf5a4433432b3d78900a62caaa9ef43eacb
William Walsh <wmdwalsh0@gmail.com>
2021-12-12 18:01:48 -0500

    commit tree 03e2cd44e2d8aa5987acd0f3be282dd4fe62d633

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: (["Fiction", "Non-Fiction"]),
+    message: "is not included in the list" }
+    validates :title, exclusion: { in: (["True Facts"]),
+    message: "is not included in the list" }
+    # def avail_category?
+    #     if :category == "Fiction" || "Non-Fiction"
+    #   end
+    # end
 end
